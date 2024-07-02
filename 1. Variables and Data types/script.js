e9de4e69e107a2e75bb1d31ea6a94d7867801f1b
Faiqa Batool <115587465+Faiqa-batool@users.noreply.github.com>
2024-07-03 01:15:24 +0500

    Update script.js

--- 1. Variables and Data types/script.js
+++ 1. Variables and Data types/script.js
@@ -1,1 +1,26 @@
+// Qs 1: object of a product
 
+const product = {
+    title: "Parker Jotter Standard CT Ball Pen",
+    color: "Black",
+    rating: 4,
+    isdeal: true,
+    price: 270,
+    offer: 5
+};
+console.log(product);
+
+// Qs 2: Object of a social media profile
+
+const profile = {
+    username : "shradhakhapra",
+    posts : 195,
+    followers : 569000,
+    following: 4,
+    isfollow: false,
+    title: "Shradha Khapra",
+    bio: "Entrepreneur, Apna College | Ex-Microsoft, DRDO To educate someone is the highest privilege"
+};
+
+console.log(profile);
+console.log(typeof profile["username"]);
