252d348a6a4f6697379d413f1d47a849cb175bce
Faiqa Batool <115587465+Faiqa-batool@users.noreply.github.com>
2024-07-04 01:01:50 +0500

    Update script.js

--- 3. Loops/script.js
+++ 3. Loops/script.js
@@ -1,1 +1,34 @@
+// for-of loop:
 
+let str = "FaiqaBatool";
+
+let size = 0;
+for(let i of str){
+    console.log("i =", i);
+    size++;
+}
+
+console.log(size);
+
+//for-in loop:
+
+let student = {
+    fullName : "Faiqa Batoo",
+    age : 22,
+    cgpa: 3.1,
+    isPass: true
+};
+
+for(let key in student){
+    console.log("key =", key, " value =", student[key]);
+    //here key is a variable that's why no inverted commas
+}
+
+// Qs 1: Print all even numbers from 0 to 100:
+
+let n= 100;
+for(let i=0; i<=n+1;i++){
+    if(i%2===0){    //even number
+        console.log(i, " is an even number");
+    }
+}
