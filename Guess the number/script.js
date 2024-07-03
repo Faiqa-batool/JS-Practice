ad15f61b02cf75646039fa207972ae2a03bf37d5
Faiqa Batool <115587465+Faiqa-batool@users.noreply.github.com>
2024-07-04 01:05:13 +0500

    Update script.js

--- Guess the number/script.js
+++ Guess the number/script.js
@@ -1,1 +1,12 @@
+// Qs 2: guess the game number:
 
+let gameNum = 22;
+
+let userNum = prompt("Guess the game number: ");
+console.log("User Number is ", userNum);
+
+
+while(userNum != gameNum){
+    userNum = prompt("You guessed the wrong number. Guess the game number again: ");
+}
+console.log("Congratulations! You enetered the correct number");
