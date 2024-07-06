79fd8e2fdf46392a87b9208b479f2e9991cc15a1
Faiqa Batool <115587465+Faiqa-batool@users.noreply.github.com>
2024-07-07 01:09:04 +0500

    Update index.js

--- 6. Functions/index.js
+++ 6. Functions/index.js
@@ -1,1 +1,14 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JS Learning | Functions</title>
+</head>
+<body>
+    <h1>Chapter 5</h1>
+    <h2>Functions</h2>
+    <h3>open console and see the output</h3>
+    <script src="./script.js"></script>
+</body>
+</html>
