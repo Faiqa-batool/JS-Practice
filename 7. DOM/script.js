8b2e205db030a5aa94402ab8b92bdaa526e37c6f
Faiqa Batool <115587465+Faiqa-batool@users.noreply.github.com>
2024-07-07 01:21:04 +0500

    Update script.js

--- 7. DOM/script.js
+++ 7. DOM/script.js
@@ -1,1 +1,16 @@
+let div = document.querySelector("div");
+console.dir(div);
 
+// div.innerText = "Faiqa is a Cutie ^-^"
+
+div.innerText = "<div>inner div</div>"
+
+let heading = document.querySelector("h1");
+console.dir(heading);
+heading.innerText = "FAIQA!!! YOU CUTIEE!! ^-^";
+
+heading.innerHTML = "<i>FAIQA!!! YOU CUTIEE!!</i> ^-^";
+// can write tags in it as well
+
+// heading.textContent;
+// it shows the hidden tag's content
