10c7379f76dd529bb014802eb051d6b0d75b3792
Faiqa Batool <115587465+Faiqa-batool@users.noreply.github.com>
2024-07-08 00:47:54 +0500

    Update index.js

--- DOM Practice Question/index.js
+++ DOM Practice Question/index.js
@@ -1,1 +1,13 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DOM Practice</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <p class="class1">This is a paragraph tag.</p>    
+    <script src="./script.js"></script>
+</body>
+</html>
