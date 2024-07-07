8b59ea883b1d3a3bf40af9e22d47a1c9f1e111e2
Faiqa Batool <115587465+Faiqa-batool@users.noreply.github.com>
2024-07-08 00:48:54 +0500

    Update script.js

--- DOM Practice Question/script.js
+++ DOM Practice Question/script.js
@@ -1,1 +1,26 @@
+// Qs 1 : Create a new button element. Give it a text “click me”, 
+// background color of red & text color of white.
+// and Insert the button as the first element inside the body tag.
+
+// create a new button
+let newBtn = document.createElement("button");
+
+// give it a text "click me"
+newBtn.innerText = "Click Me";
+newBtn.style.height = "40px";
+
+// background color of red
+newBtn.style.backgroundColor = "red";
+
+// text color of white
+newBtn.style.color = "white";
+
+// Insert the button as the first element inside the body tag.
+document.querySelector("body").prepend(newBtn);
+
+// Qs 2: create a new class in CSS and try to append this class to the 
+// <p> element.
+
+let para = document.querySelector("p");
+para.classList.add("newClass");
 
