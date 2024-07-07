020eef1c985a9623c4f7092f1ddafb3ae064dd5a
Faiqa Batool <115587465+Faiqa-batool@users.noreply.github.com>
2024-07-08 00:46:31 +0500

    Update script.js

--- Hello JS/script.js
+++ Hello JS/script.js
@@ -1,1 +1,20 @@
 
+let heading = document.querySelector("h2");
+
+heading.innerHTML += " from Apna College students";
+
+let boxes = document.querySelectorAll(".box");
+
+// let texts = ["Hi", "This is", "Faiqa Batool"];
+
+// boxes.forEach((div, index) => {
+//   // Make sure we have enough texts for the number of divs
+//   if (index < texts.length) {
+//     div.textContent = texts[index];
+//   }
+// });
+let idx = 1;
+for (let box of boxes){
+    box.innerText += `${idx}`;
+    idx++;
+}
