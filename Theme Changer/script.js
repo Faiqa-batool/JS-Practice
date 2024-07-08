6c840a19e2e598bb0cd33b35dc807d6d5bd386bd
Faiqa Batool <115587465+Faiqa-batool@users.noreply.github.com>
2024-07-09 01:03:55 +0500

    Update script.js

--- Theme Changer/script.js
+++ Theme Changer/script.js
@@ -1,1 +1,18 @@
+let modeBtn = document.querySelector("#mode");
+let body = document.querySelector("body")
 
+let currMode = "light";
+
+modeBtn.addEventListener("click", ()=> {
+    if (currMode === "light") {
+        currMode = "dark";
+        body.classList.remove("light");
+        body.classList.add("dark");
+    } else {
+        currMode = "light";
+        body.classList.remove("dark");
+        body.classList.add("light");
+        
+    }
+    console.log(currMode);
+});
