914003a7fa4640a8a505e6524006c6983caf8d47
Faiqa Batool <115587465+Faiqa-batool@users.noreply.github.com>
2024-07-09 01:00:19 +0500

    Update script.js

--- 9. Events/script.js
+++ 9. Events/script.js
@@ -1,1 +1,28 @@
+// event handling
 
+let btn1 = document.querySelector("#btn1");
+
+// btn1.onclick = () => {
+//     console.log("You clicked on a button in JS");
+//     let a = 0;
+//     a++;
+//     console.log(a);
+// }
+
+let box = document.querySelector("div");
+
+box.onmouseover = () => {
+    console.log("You are inside div 2");
+}
+
+let handler1 = ()=>{
+    console.log("Btn1 was clicked - handler 1");
+}
+// event listner
+btn1.addEventListener("click", handler1);
+
+btn1.addEventListener("click", ()=>{
+    console.log("Btn1 was clicked - handler 2");
+});
+
+btn1.removeEventListener("click", handler1);
