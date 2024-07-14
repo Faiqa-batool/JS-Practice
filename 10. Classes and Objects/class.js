d8fef69b072db28d8735bda6302371ee0c59ee62
Faiqa Batool <115587465+Faiqa-batool@users.noreply.github.com>
2024-07-14 22:30:28 +0500

    Update class.js

--- 10. Classes and Objects/class.js
+++ 10. Classes and Objects/class.js
@@ -1,1 +1,43 @@
+class toyotaCar{
+    constructor(brand, mileage){
+        // things we want to do while initialization
+        console.log("creating new object");
+        this.brand = brand;
+        this.mileage = mileage;
+    }
+    start(){
+        console.log("start");
+    }
+
+    stop(){
+        console.log("stop");
+    }
+
+    // setBrand(brand){
+    //     this.brandName = brand;
+    // }
+}
+
+// creating object
+let fortuner = new toyotaCar();
+console.log(fortuner);
+let corrolla = new toyotaCar("corrolla", 240);
+console.log(corrolla);
+
+class Parent{
+    setEyeColor(eyeColor){
+        this.eyeColor = eyeColor;
+    }
+    hello(){
+        console.log("say hello");
+    }
+}
+
+class Child extends Parent{
+    useTech(){
+        console.log("can use technology");
+    }
+}
+
+let paras = new Child();
 
