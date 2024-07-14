0c91a9a1a24186d77fbff637a9dcc6d561094078
Faiqa Batool <115587465+Faiqa-batool@users.noreply.github.com>
2024-07-14 22:31:30 +0500

    Update engineer.js

--- 10. Classes and Objects/engineer.js
+++ 10. Classes and Objects/engineer.js
@@ -1,1 +1,43 @@
+class Person{
+    constructor(name){
+        console.log("enter parent constructor");
+        this.specie = "homo sapiens";
+        this.name = name;
+    }
+    eat(){
+        console.log("eat");
+    }
 
+    sleep(){
+        console.log("sleep");
+    }
+
+    work(){
+        console.log("do nothing"); 
+    }
+}
+
+class Engineer extends Person{
+    constructor(name){
+        console.log("enter child constructor");
+        super(name);
+        // this.branch = branch;
+        console.log("exit child constructor");
+    }
+    work(){
+        console.log("Solve problems, build something");
+    }
+}
+
+class Doctor extends Person{
+    work(){
+        console.log("treat patients");
+    }
+}
+
+let Faiqa = new Engineer("Faiqa");
+Faiqa.work();
+// Faiqa.super.work();
+
+// let Ayesha = new Doctor();
+// Ayesha.work();
